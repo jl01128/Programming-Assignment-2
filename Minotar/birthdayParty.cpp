38b8f0feaa508f334da64a26eb9e8b406280ca4b
jl01128 <jackie01128@gmail.com>
2023-02-23 20:53:54 -0500

    program file for both questions

---
+++ Minotar/birthdayParty.cpp
@@ -0,0 +1,140 @@
+//Jackie Lin 
+//COP4520 - CONCEPTS PARALLEL DISTRIBUTED
+// 02/22/2023
+
+#include <iostream>
+#include <array>
+#include <chrono>
+#include <mutex>
+#include <time.h>
+#include <algorithm>
+#include <random>
+
+//for our thread
+#include <thread>
+
+#define GUEST_NUM 25
+
+//Tracks the guests tthat have eaten cupacak in laybrinth
+std::array<bool, GUEST_NUM> chosenGuests;
+std::mutex mutex;
+
+//checks if cupcake is present in laybrinth
+bool cupcakePresent = true;
+
+//tracks our number of guests
+int counter = 0;
+
+//Tracks the current guest in laybrinth
+int currentThread;
+
+//function prototype
+void guestInLaybrinth(unsigned int threadInd);
+void cupCakeChecker();
+int randNum(int min, int max);
+
+int main(){
+
+    //start of our timer 
+    //double start = clock();
+    auto start = std::chrono::high_resolution_clock::now();
+
+    //declare our threads for given number of guests
+    std::array<std::thread, GUEST_NUM> threads{};
+
+    
+    //first thread sets up counter and checks if cupcake is present
+    threads[0] = std::thread(cupCakeChecker);
+
+    //set all threads(guest) to enter laybrinth
+    for(int i = 1; i < threads.size(); i++) {
+        threads[i] = std::thread(guestInLaybrinth, i);
+    }
+
+
+    //continue to generate number until all guest have entered laybrinth
+    while(counter < GUEST_NUM) {
+        currentThread = randNum(0, GUEST_NUM);
+    }
+    
+
+    //join our threads
+    for(int i = 0; i < GUEST_NUM; i++) {
+		threads[i].join();
+	}
+
+    //end of our timer
+    //double end = clock();
+    auto end = std::chrono::high_resolution_clock::now();
+
+    //total time to run threads
+	//double time = (end - start);
+    auto time = std::chrono::duration<double, std::milli>(end - start);
+
+    std::cout << std::endl;
+    std::cout << "Total count of guests who entered Laybrinth: " << counter << std::endl;
+    std::cout << "Time: " << time.count() << "s" << std::endl;
+
+}
+
+//guests enter laybrinth function call
+void guestInLaybrinth(unsigned int threadInd) {
+    
+    //tracks the number of guests that have enter laybrinth
+    while(counter < GUEST_NUM){
+        //lock
+        mutex.lock();
+
+        //marks current thread to see if enter laybrinth and if cupcake present
+        if(currentThread == threadInd && cupcakePresent && !chosenGuests[threadInd]) {
+            cupcakePresent = false;
+            //guest taken cupcake
+            chosenGuests[threadInd] = true;
+            //guest has enter laybrinth
+            std::cout << "Guest Number(" << threadInd << ") entered laybrinth and ate the cupcake" << std::endl;
+        }
+
+        //unlock
+        mutex.unlock();
+    }
+}
+//end of function
+
+//used by first thread set to count guest
+void cupCakeChecker(){
+    
+    //keeps track of number guest number
+    while(counter < GUEST_NUM) {
+        mutex.lock();
+
+        //start from first thread
+        if(currentThread == 0){
+           
+            //cupcake not present, add to count and set new cupcake
+            if(!cupcakePresent){
+                counter++;
+                //added new cupcake
+                cupcakePresent = true;
+            }
+            
+            //make sures that every given thread(guests has taken cupcake)
+            //whihc we then add to count and set new cake
+            if(cupcakePresent && !chosenGuests[0]){
+                counter++;
+                cupcakePresent = true;
+                //added new cupcake
+                chosenGuests[0] = true;
+                // guest has taken cupcake
+            }
+        }
+        //unlcok
+        mutex.unlock();
+    }
+}
+//end of cupcake checker in laybrinth
+
+//generates random number num for guest to enter
+int randNum(int min, int max) {
+	return rand() % (max - min + 1) + min;
+}
+//end of function
---
+++ Minotar/crystalVase.cpp
@@ -0,0 +1,103 @@
+#include <iostream>
+#include <array>
+#include <deque>
+#include <chrono>
+#include <time.h>
+#include <mutex>
+#include <unordered_set>
+#include <random>
+
+//for out thread
+#include <thread>
+
+//concrete number of guests
+#define GUEST_NUM 25
+
+//Sign on door stsatus
+enum Sign {
+    AVAILABLE,
+    BUSY
+};
+
+//Tracks guests who seen case
+std::unordered_set<std::thread::id> guestsVisited{};
+std::mutex mutex;
+
+//decalre showrooom status, which is initalized to Available from start
+Sign showRoomStatus = Sign::AVAILABLE;
+
+//function prototype
+void seeVase(int threadInd); 
+int randNum(int min, int max);
+
+
+int main() {
+    //start of our timer 
+    //double start = clock();
+    auto start = std::chrono::high_resolution_clock::now();
+
+    //declare our threads
+    std::array<std::thread, GUEST_NUM> threads{};
+
+    //let threads(guests) see vase using method 2
+    for(int i = 0; i < threads.size(); i++) {
+        threads[i] = std::thread(seeVase, i);
+    }
+
+    //join our threads
+    for(int i = 0; i < GUEST_NUM; i++) {
+		threads[i].join();
+	}
+
+
+    //end of our timer
+    //double end = clock();
+    auto end = std::chrono::high_resolution_clock::now();
+
+    //total time to run threads
+	//double time = (end - start);
+    auto time = std::chrono::duration<double, std::milli>(end - start);
+    
+    std::cout << std::endl;
+    std::cout << "Total count of guests who viewed the Minotar's crystal vase." << std::endl;
+    std::cout << "Time: " << time.count() << "ms" << std::endl;
+}
+
+//current thread(guest) is sent to view vase
+void seeVase(int threadInd){
+    //set current thread(guest) 
+    std::thread::id threadId = std::this_thread::get_id();
+
+    //number of guest who seen vase is tracked
+    while(guestsVisited.size() < GUEST_NUM){
+        mutex.lock();
+        //lock
+
+        //check if room show room is avaible and whehter or not that guest has already seen the vase
+        if(showRoomStatus == Sign::AVAILABLE && guestsVisited.find(threadId) == guestsVisited.end()){
+            //current thread(guest) will set show room sign to busy, so no one is to enter
+            showRoomStatus = Sign::BUSY;
+            
+            std::cout << "Guest Number(" << threadInd << ") is viewing the Minotar's crystal vase" << std::endl;
+            
+            //(testing purposes of potential realistic viewing time frame)
+            //delay thread time for how long the given guest is viewing the vase
+            //std::this_thread::sleep_for(std::chrono::milliseconds(randNum(1000, 10000)));
+
+            //current thread(guest) then sets singn to avaible and leaves show room after seeing case
+            showRoomStatus = Sign::AVAILABLE;
+            
+            //set the current thread(guest) to have seen case
+            guestsVisited.insert(threadId);
+        }
+
+        mutex.unlock();
+        //unlock
+    }
+}
+
+///generates random number num for guest to enter
+int randNum(int min, int max){
+	return rand() % (max - min + 1) + min;
+}
+//end of function
